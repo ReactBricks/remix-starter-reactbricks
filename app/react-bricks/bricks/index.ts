d0f9bf7f868ff720c33561d2f48817e8edd27b9d
Giorgio Tognoli <giorgio@f2.net>
2022-05-26 14:44:21 +0200

    removed react-bricks-ui

--- app/react-bricks/bricks/index.ts
+++ app/react-bricks/bricks/index.ts
@@ -1,13 +1,9 @@
 import { types } from 'react-bricks/frontend'
-import website from 'react-bricks-ui/website'
-import blog from 'react-bricks-ui/blog'
 
 // Example brick
 import HeroUnit from './HeroUnit'
 
 const bricks: types.Brick<any>[] = [
-  ...website,
-  ...blog,
   // Put here your other bricks...
   HeroUnit,
 ]
