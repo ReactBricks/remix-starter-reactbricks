132b04adec26828d297bdb03e138bd0c83bf61ce
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-04-07 10:46:48 +0200

    header classes fixed

--- app/react-bricks/bricks/layout/Header.tsx
+++ app/react-bricks/bricks/layout/Header.tsx
@@ -1,14 +1,14 @@
-import React, { useContext, useRef, useState } from "react"
+import React, { useContext, useRef, useState } from 'react'
 import {
   Image,
   Repeater,
   types,
   Link,
   ReactBricksContext,
-} from "react-bricks/frontend"
-import { BsMoonFill, BsSunFill } from "react-icons/bs"
-import { FiMenu, FiX } from "react-icons/fi"
-import useOnClickOutside from "./useClickOutside"
+} from 'react-bricks/frontend'
+import { BsMoonFill, BsSunFill } from 'react-icons/bs'
+import { FiMenu, FiX } from 'react-icons/fi'
+import useOnClickOutside from './useClickOutside'
 
 interface HeaderProps {}
 
@@ -21,38 +21,38 @@ const Header: types.Brick<HeaderProps> = ({}) => {
   useOnClickOutside(ref, () => setMobileMenuOpen(false))
 
   return (
-    <section className={"HeaderSection"}>
-      <nav className={"HeaderNavClass"}>
-        <Link href='/' aria-label='home' className={"HeaderLinkLogo"}>
+    <section className={'HeaderSection'}>
+      <nav className={'HeaderNavClass'}>
+        <Link href="/" aria-label="home" className={'HeaderLinkLogo'}>
           <Image
-            propName='logo'
-            alt='Logo'
+            propName="logo"
+            alt="Logo"
             maxWidth={300}
-            imageClassName={"HeaderImageClass"}
+            imageClassName={'HeaderImageClass'}
           />
         </Link>
-        <div className={"HeaderContainerMenuItems"}>
-          <Repeater propName='menuItems' />
+        <div className={'HeaderContainerMenuItems'}>
+          <Repeater propName="menuItems" />
         </div>
-        <div className={"HeaderContainerButtons"}>
+        <div className={'HeaderContainerButtons'}>
           <Repeater
-            propName='buttons'
+            propName="buttons"
             renderWrapper={(items) => (
-              <div className={"HeaderButtonsWrapper"}>{items}</div>
+              <div className={'HeaderButtonsWrapper'}>{items}</div>
             )}
           />
         </div>
 
-        <div ref={ref} className={"HeaderContainerHamburgerMenu"}>
+        <div ref={ref} className={'HeaderContainerHamburgerMenu'}>
           {/* DARK MODE BUTTON MOBILE */}
           <a
-            type='button'
-            className={"darkModeButtonMobile"}
+            type="button"
+            className={'darkModeButtonMobile'}
             onClick={toggleColorMode}
           >
             {!isDarkColorMode ? (
               <BsSunFill
-                style={{ fontSize: "1.25rem", lineHeight: "1.75rem" }}
+                style={{ fontSize: '1.25rem', lineHeight: '1.75rem' }}
               />
             ) : (
               <BsMoonFill />
@@ -60,26 +60,30 @@ const Header: types.Brick<HeaderProps> = ({}) => {
           </a>
 
           <button
-            className={"HeaderButtonHamburgerMenu"}
+            className={'HeaderButtonHamburgerMenu'}
             onClick={() => setMobileMenuOpen((current) => !current)}
           >
-            {mobileMenuOpen ? <FiX /> : <FiMenu />}
+            {mobileMenuOpen ? (
+              <FiX size={18} className={'HamburgerMenuFiX'} />
+            ) : (
+              <FiMenu size={20} className={'HamburgerMenuFiMenu'} />
+            )}
           </button>
           {mobileMenuOpen && (
-            <div className={"HeaderContainerHamburgerMenuItems"}>
-              <Repeater propName='menuItems' />
+            <div className={'HeaderContainerHamburgerMenuItems'}>
+              <Repeater propName="menuItems" />
             </div>
           )}
         </div>
 
         {/* DARK MODE BUTTON DESKTOP */}
         <a
-          type='button'
-          className={"darkModeButtonDesktop"}
+          type="button"
+          className={'darkModeButtonDesktop'}
           onClick={toggleColorMode}
         >
           {!isDarkColorMode ? (
-            <BsSunFill style={{ fontSize: "1.25rem", lineHeight: "1.75rem" }} />
+            <BsSunFill style={{ fontSize: '1.25rem', lineHeight: '1.75rem' }} />
           ) : (
             <BsMoonFill />
           )}
@@ -90,22 +94,22 @@ const Header: types.Brick<HeaderProps> = ({}) => {
 }
 
 Header.schema = {
-  name: "header",
-  label: "Header",
-  category: "layout",
-  tags: ["header", "menu"],
+  name: 'header',
+  label: 'Header',
+  category: 'layout',
+  tags: ['header', 'menu'],
   repeaterItems: [
     {
-      name: "menuItems",
-      itemType: "header-menu-item",
-      itemLabel: "Item",
+      name: 'menuItems',
+      itemType: 'header-menu-item',
+      itemLabel: 'Item',
       min: 0,
       max: 6,
     },
     {
-      name: "buttons",
-      itemType: "button",
-      itemLabel: "Button",
+      name: 'buttons',
+      itemType: 'button',
+      itemLabel: 'Button',
       min: 0,
       max: 2,
     },
@@ -114,43 +118,43 @@ Header.schema = {
   getDefaultProps: () => ({
     menuItems: [
       {
-        linkPath: "/",
-        linkText: "Home",
+        linkPath: '/',
+        linkText: 'Home',
       },
       {
-        linkPath: "/about-us",
-        linkText: "About us",
+        linkPath: '/about-us',
+        linkText: 'About us',
       },
       {
-        linkPath: "",
-        linkText: "Features",
+        linkPath: '',
+        linkText: 'Features',
         submenuItems: [
           {
-            linkText: "Visual editing",
+            linkText: 'Visual editing',
             linkDescription:
-              "The best visual experience for your content editors",
-            linkPath: "/",
+              'The best visual experience for your content editors',
+            linkPath: '/',
           },
         ],
       },
     ],
     logo: {
-      src: "https://images.reactbricks.com/original/8d0eb40f-6e1a-4f6c-9895-a06767fcf5fa.svg",
+      src: 'https://images.reactbricks.com/original/8d0eb40f-6e1a-4f6c-9895-a06767fcf5fa.svg',
       placeholderSrc:
-        "https://images.reactbricks.com/original/8d0eb40f-6e1a-4f6c-9895-a06767fcf5fa.svg",
-      srcSet: "",
+        'https://images.reactbricks.com/original/8d0eb40f-6e1a-4f6c-9895-a06767fcf5fa.svg',
+      srcSet: '',
       width: 450,
       height: 100,
-      alt: "React Bricks",
-      seoName: "react-bricks",
+      alt: 'React Bricks',
+      seoName: 'react-bricks',
     },
     buttons: [
       {
-        text: "Edit content",
-        href: "/admin",
+        text: 'Edit content',
+        href: '/admin',
         isTargetBlank: false,
-        type: "solid",
-        padding: "small",
+        type: 'solid',
+        padding: 'small',
       },
     ],
   }),
