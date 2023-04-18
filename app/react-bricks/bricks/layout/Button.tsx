4fafd912023aa60bef241a78c1fa5fc1f3b62675
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-04-18 15:38:01 +0200

    Button : fixed props

--- app/react-bricks/bricks/layout/Button.tsx
+++ app/react-bricks/bricks/layout/Button.tsx
@@ -88,7 +88,7 @@ Button.schema = {
     href: '',
     isTargetBlank: false,
     buttonType: 'submit',
-
+    simpleAnchorLink: false,
     variant: 'solid',
     padding: 'normal',
   }),
@@ -118,6 +118,12 @@ Button.schema = {
         {
           name: 'isTargetBlank',
           label: 'Open in new window',
+          type: types.SideEditPropType.Boolean,
+          show: (props) => props.type === 'link',
+        },
+        {
+          name: 'simpleAnchorLink',
+          label: 'Simple anchor (no SPA link)',
           type: types.SideEditPropType.Boolean,
           show: (props) => props.type === 'link',
         },
