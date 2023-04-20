46b64e42057fb7a3180ec70297e3b9530d30d04b
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-04-20 15:46:08 +0200

    import fixed

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react"
-import { useReactBricksContext } from "react-bricks"
+import { ReactNode } from 'react'
+import { useReactBricksContext } from 'react-bricks/frontend'
 
 interface LayoutProps {
   children?: ReactNode
@@ -9,8 +9,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const { isDarkColorMode } = useReactBricksContext()
 
   return (
-    <div className={`layoutContainer ${isDarkColorMode ? "dark" : "light"}`}>
-      <main className='layoutMain'>{children}</main>
+    <div className={`layoutContainer ${isDarkColorMode ? 'dark' : 'light'}`}>
+      <main className="layoutMain">{children}</main>
     </div>
   )
 }
